c1128d4397f26886682ed9f75c582d6592fcb98c
Kyle Bittinger <kylebittinger@gmail.com>
2018-09-21 23:12:03 -0400

    Fix setup file for PyPI

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
-#!/usr/bin/env python
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='BLAST READ and OTU Consensus Classifier',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='brocc',
     version='1.4.0',
-    description='BLAST READ and OTU Consensus Classifier',
     author='Serena Dollive, Kyle Bittinger',
     author_email='kylebittinger@gmail.com',
     maintainer='Kyle Bittinger',
     maintainer_email='kylebittinger@gmail.com',
-    url='',
+    description='BLAST READ and OTU Consensus Classifier',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kylebittinger/brocc',
     packages=['brocclib'],
     scripts=[
         'scripts/brocc.py', 'scripts/create_local_taxonomy_db.py',
         'scripts/compare_brocc_assignments.py'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
 )
